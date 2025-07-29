bcb5826a65bd9699fd12283f2c82ed424e6f23df
Harsh Gandhi <harshgandhi.it.nbnstic@gmail.com>
2025-07-30 00:02:15 +0530

    Java

---
+++ TNSJavaPracticeCodes/src/practice.java
@@ -0,0 +1,11 @@
+
+public class practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int y=15;
+			System.out.println("Hii Ecllipse");
+			System.out.println(y);
+	}
+
+}
